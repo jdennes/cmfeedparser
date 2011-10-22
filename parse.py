565a80170378ee51fa0b5514020baa92f11d31ff
James Dennes <jdennes@gmail.com>
2011-10-22 16:37:39 +1100

    Runs on GAE now. :)

--- parse.py
+++ parse.py
@@ -1,11 +1,13 @@
 # Give me a feed url, and I'll attempt to parse it for you and give you JSON
-# usage: /parse?url=http://example.com/feed
-# dependencies: python 2.6+, feedparser, and web.py
+# Usage: /?url=http://example.com/feed
+# Dependencies: python 2.6+, feedparser, and web.py
+# Now runs on GAE, using web.py rather than webapp2 (so it can easily be run elsewhere)
 
-import time
+from google.appengine.ext.webapp.util import run_wsgi_app
 import web
 import json
 import feedparser
+import time
 
 def to_json(python_object):
   if isinstance(python_object, time.struct_time):
@@ -13,9 +15,7 @@ def to_json(python_object):
             '__value__': time.asctime(python_object)}
   raise TypeError(repr(python_object) + ' is not JSON serializable')
 
-urls = (
-  '/parse', 'Parser'
-)
+urls = ('/', 'Parser')
 app = web.application(urls, globals())
 
 class Parser:
@@ -23,12 +23,11 @@ class Parser:
     web.header('Content-Type', 'application/json')
     try:
       url = web.input().url
-      print "Attempting to parse: %s" % url
       return json.dumps(feedparser.parse(url), default=to_json)
     except Exception as e:
-      print e
       web.ctx.status = "400 Bad Request"
-      return json.dumps({"message": "Sorry, i couldn't parse that feed."})
+      return json.dumps({"message": "Sorry, i couldn't parse that feed. Usage: /?url=http://example.com/feed"})
 
 if __name__ == "__main__":
-  app.run()
+  application = app.wsgifunc()
+  run_wsgi_app(application)
